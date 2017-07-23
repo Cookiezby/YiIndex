54fb8e8ef564bcf623fe3691c30632731c9cc1df
Cookiezby <zhubingyi1992@gmail.com>
2017-07-23 18:36:07 +0900

    add strToUppercaseLetters

--- YiIndex/Util/StringUtil.swift
+++ YiIndex/Util/StringUtil.swift
@@ -9,19 +9,41 @@
 import Foundation
 
 class StringUtil {
-    
-    static func dataToCapticaLetters(data: [String], level: Int) -> [String]{
+    static func strListToUppercaseLetters(data: [String], level: Int) -> [String]{
         var result = [String]()
         for str in data {
-            var newStr = ""
-            let enRegex = "^[A-Za-z]+$"
-            for i in 0 ..< str.characters.count{
-                
-            }
-            result.append(newStr)
+            result.append(strToUppercaseLetters(str: str, level: level))
         }
         return result
     }
     
-    
+    static func strToUppercaseLetters(str: String, level: Int) -> String {
+        var transferedStr = ""
+        let upperStr = str.uppercased()
+        let upperStrUni = upperStr.unicodeScalars
+        let endIndex = upperStr.characters.count < level ? upperStr.characters.count : level
+        for i in 0 ..< endIndex {
+            let char = upperStr[upperStr.index(upperStr.startIndex, offsetBy: i)]
+            let charUni = upperStrUni[upperStrUni.index(upperStrUni.startIndex, offsetBy: i)]
+            if CharacterSet.uppercaseLetters.contains(charUni) {
+                transferedStr.append(char)
+            }else if ("\u{4E00}" <= char  && char <= "\u{9FA5}") {
+                // deal with chinese here first transfer chinese to pinyin
+                let py = NSMutableString(string: String(char))
+                CFStringTransform(py, nil, kCFStringTransformMandarinLatin, false)
+                CFStringTransform(py, nil, kCFStringTransformStripDiacritics, false)
+                let pyStr = String(py)
+                // get the first char
+                transferedStr.append(pyStr.uppercased()[pyStr.startIndex])
+            }else {
+                transferedStr.append("Z")
+            }
+        }
+        if endIndex < level {
+            for _ in 0 ..< (level - endIndex) {
+                transferedStr.append("A")
+            }
+        }
+        return transferedStr
+    }
 }
